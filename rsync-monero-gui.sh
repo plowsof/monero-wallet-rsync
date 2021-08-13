111192a5d5980ae07390153c1fff3505ed805e01
plowsof <77655812+plowsof@users.noreply.github.com>
2021-08-13 22:14:33 +0100

    Create rsync-monero-gui.sh

---
+++ rsync-monero-gui.sh
@@ -0,0 +1,4 @@
+rsync -a root@eeebox:/var/log/monero/wallets/general-fund/ /home/human/Monero/wallets/general-fund/
+flatpak run org.getmonero.Monero
+
+#you would run this file from a (in ubuntu) a .desktop shortcut
