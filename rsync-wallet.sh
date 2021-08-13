b62d434c325972a28ebbaedf06db4fcf7325e4c9
plowsof <77655812+plowsof@users.noreply.github.com>
2021-08-13 16:12:16 +0100

    Update rsync-wallet.sh

--- rsync-wallet.sh
+++ rsync-wallet.sh
@@ -1,1 +1,11 @@
-
+dir_monerujo=/data/data/com.m2049r.xmrwallet/files/wallets
+dir_wallet=$dir_monerujo
+dir_r_wallet=root@eeebox:/var/log/monero/wallets/general-fund/
+#sync the wallet files
+sudo rsync -a $dir_r_wallet $dir_wallet
+#get the owner of 'monero' folder to set file ownerships correctly
+user=$(sudo stat -c "%U" /data/data/com.m2049r.xmrwallet/files/monero)
+echo $dir_wallet
+sudo chown -R ${user}:${user} $dir_wallet
+#start monerujo (todo: find correct name)
+#am start --user 0 -n com.termux/com.termux.app.TermuxActivity
