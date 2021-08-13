296b31a9738980ea2eccefe9ed4057feff978448
plowsof <77655812+plowsof@users.noreply.github.com>
2021-08-13 23:07:16 +0100

    Update rsync-wallet.sh

--- rsync-wallet.sh
+++ rsync-wallet.sh
@@ -5,7 +5,6 @@ dir_r_wallet=root@eeebox:/var/log/monero/wallets/general-fund/
 sudo rsync -a $dir_r_wallet $dir_wallet
 #get the owner of 'monero' folder to set file ownerships correctly
 user=$(sudo stat -c "%U" /data/data/com.m2049r.xmrwallet/files/monero)
-echo $dir_wallet
 sudo chown -R ${user}:${user} $dir_wallet
 #start monerujo 
 #https://github.com/m2049r/xmrwallet/blob/9ed92e51175338d958e910d600a34f0a0178d483/app/src/main/AndroidManifest.xml#L24
